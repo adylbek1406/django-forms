6fae61da69b1fd2465583fbf75799f011ec885c7
adylbek1406 <gazbekovadilet9@gmail.com>
2025-04-17 16:37:30 +0600

    первый коммит

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def postuser(request):
+    # получаем из данных запроса POST отправленные через форму данные
+    name = request.POST.get("name", "Undefined")
+    age = request.POST.get("age", 1)
+    return HttpResponse(f"<h2>Name: {name}  Age: {age}</h2>")
